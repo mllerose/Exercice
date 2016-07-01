f0627af482bd744f01a7fa42f621059f5c05e83f
Matthieu Mota <matthieumota@gmail.com>
2016-07-01 11:54:09 +0200

    Autoload composer

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+if(file_exists('vendor/autoload.php')){
+	require 'vendor/autoload.php';
+}else{
+	echo "Vous devez lancer un composer install.";
+}
+
 if(file_exists('config.php')){
 	require 'config.php';
 }else{
